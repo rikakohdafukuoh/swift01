ed91d729a232fcfbe761528b8ee0eca128ce524b
rika.kohda <>
2023-09-04 21:56:45 +0900

    初期画面実装＆色拡張　16進数対応

---
+++ HelloSwiftUI/HelloSwiftUI/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  HelloSwiftUI
+//
+//  Created by rika.kohda on 2023/09/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var str = "Hello,cats"
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text(str)
+                .foregroundColor(Color(0xFF00FF, alpha: 1.0))
+            Button("猫が増えるボタン") {
+                str = "Welcome,cats!"
+                print("🐈")
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloSwiftUI/HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by rika.kohda on 2023/09/04.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
