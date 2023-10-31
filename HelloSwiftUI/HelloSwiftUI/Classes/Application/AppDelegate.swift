7105a053c95ac8029eb46a15c66da42c0068a8a2
jun.kohda <jun.koda@mazurica.com>
2023-11-01 00:53:33 +0900

    エントリポイント変更

--- HelloSwiftUI/HelloSwiftUI/Classes/Application/AppDelegate.swift
+++ HelloSwiftUI/HelloSwiftUI/Classes/Application/AppDelegate.swift
@@ -9,6 +9,7 @@ import OHHTTPStubs
 import OHHTTPStubsSwift
 import UIKit
 
+@main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         mock()
