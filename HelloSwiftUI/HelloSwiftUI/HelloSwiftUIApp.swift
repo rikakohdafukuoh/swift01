30e69face4f75adf29ef383f8833fb88315102bc
jun.kohda <jun.koda@mazurica.com>
2023-10-31 22:29:11 +0900

    CounterViewの仮実装

--- HelloSwiftUI/HelloSwiftUI/HelloSwiftUIApp.swift
+++ HelloSwiftUI/HelloSwiftUI/HelloSwiftUIApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct HelloSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            //ContentView()
+            CounterView()
         }
     }
 }
