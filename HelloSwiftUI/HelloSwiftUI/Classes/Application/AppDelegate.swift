3521dcffeabc74ae93f7b5b54f61aff79c5df997
jun.kohda <jun.koda@mazurica.com>
2023-10-31 23:21:19 +0900

    ビルドが通るように修正

--- HelloSwiftUI/HelloSwiftUI/Classes/Application/AppDelegate.swift
+++ HelloSwiftUI/HelloSwiftUI/Classes/Application/AppDelegate.swift
@@ -9,7 +9,6 @@ import OHHTTPStubs
 import OHHTTPStubsSwift
 import UIKit
 
-@main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         mock()
